95ea22f65556d8359259128e839ac42a353b8a1c
Llohee <nggiang141@gmail.com>
2024-04-09 21:14:54 +0700

    add api login

--- src/component/login_register/Student/loginform.jsx
+++ src/component/login_register/Student/loginform.jsx
@@ -1,46 +1,88 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import Loginsvg_6 from "../../../images/Loginsvg_6.svg";
 import Loginsvg_7 from "../../../images/Loginsvg_7.svg";
+import { Row, Col, Form } from "antd";
+import { useLoginForm } from "./hook";
 import { useNavigate } from "react-router-dom";
 
 const LoginStudentForm = () => {
   const navigate = useNavigate();
+
+  const { handleFormSubmit, onFinishFailed, success } = useLoginForm();
+  useEffect(() => {
+    if (success) {
+      navigate("/home");
+    }
+  }, [success, navigate]);
   return (
-    <div className="w-96 flex flex-col items-center justify-center gap-16">
-      <div className="w-full flex flex-col gap-6">
-        <div className="flex gap-2 px-3 rounded border border-white">
-          <img src={Loginsvg_6} alt="" />
-          <input
-            placeholder="TÊN ĐĂNG NHẬP"
-            type="text"
-            onFocus={null}
-            className="p-2 bg-inherit focus:outline-none w-full text-white"
-          ></input>
-        </div>
-        <div className="flex gap-2 px-3 rounded border border-white">
-          <img src={Loginsvg_7} alt="" />
-          <input
-            placeholder="MẬT KHẨU"
-            type="text"
-            onFocus={null}
-            className="p-2 bg-inherit focus:outline-none w-full text-white"
-          ></input>
-        </div>
-      </div>
-      <div className="w-full flex flex-col gap-2">
-        <button className="py-2 rounded border-none bg-white text-[#2148C0] text-xl">
+    <Form
+      onFinish={handleFormSubmit}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+      className="w-96 flex flex-col items-center justify-center gap-16"
+    >
+      <Row className="w-full flex flex-col gap-6">
+        <Form.Item
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+          className="py-6 px-3 rounded border border-white"
+        >
+          <Row className="flex gap-2">
+            <Col>
+              <img src={Loginsvg_6} alt="" className="" />
+            </Col>
+            <Col>
+              <input
+                name="email"
+                placeholder="TÊN ĐĂNG NHẬP"
+                type="text"
+                onFocus={null}
+                className="h-8 bg-inherit focus:outline-none w-full text-white"
+              ></input>
+            </Col>
+          </Row>
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+          className="py-6 px-3 rounded border border-white"
+        >
+          <Row className="flex gap-2">
+            <Col>
+              <img src={Loginsvg_7} alt="" className="" />
+            </Col>
+            <Col>
+              <input
+                name="password"
+                placeholder="MẬT KHẨU"
+                type="password"
+                onFocus={null}
+                className="h-8 bg-inherit focus:outline-none w-full text-white"
+              ></input>
+            </Col>
+          </Row>
+        </Form.Item>
+      </Row>
+      <Row className="w-full flex flex-col gap-2">
+        <button
+          type="primary"
+          htmlType="submit"
+          className="py-2 rounded border-none bg-white text-[#2148C0] text-xl"
+        >
           Đăng nhập
         </button>
-        {/* <button
-          onClick={() => {
-            navigate("/register");
-          }}
-          className="bg-inherit border-none text-end text-white hover:text-sky-600"
-        >
-          Đăng ký
-        </button> */}
-      </div>
-    </div>
+      </Row>
+    </Form>
   );
 };
 
--- src/page/PrivateRoute.jsx
+++ src/page/PrivateRoute.jsx
@@ -4,16 +4,16 @@ import { Navigate, Outlet } from "react-router-dom";
 const PrivateRoute = (props) => {
   
   const userLoggedIn = () => {
-    const token = localStorage.getItem("token"); // User da login
+    const token = localStorage.getItem("token");
     if (token) return true;
     return false;
   };
 
   if (!userLoggedIn()) {
-    return <Navigate to={"login"} replace />;
+    return <Navigate to={"/"} replace />;
   }
   if (!userLoggedIn() ) {
-    return <Navigate to={"login"} replace />;
+    return <Navigate to={"/"} replace />;
   }
   
   return (
